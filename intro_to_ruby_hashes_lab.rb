77b960d98fb452725f2ca0cba87f3ba9376d6c9a
Justin Phillips <justinphil02@yahoo.com>
2019-10-02 23:20:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ def monopoly_with_third_tier
       :names => {
         :reading_railroad => {},
         :pennsylvania_railroad => {},
-        :dallas => "hipster",
+        :b_and_o_railroad => {},
         :kevin => "jock"
         }
       } 
